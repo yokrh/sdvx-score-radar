cc8fb442a8df8e549ddef990066c4035da7cdeed
yokrh <y.oka.gml@gmail.com>
2020-06-19 00:37:48 +0900

    refactor app api run on local shell

--- app/api/server/docker_image_run_on_local.sh
+++ app/api/server/docker_image_run_on_local.sh
@@ -7,6 +7,9 @@ IMAGE_VERSION=1.0.0
 # Run
 # 8080 is the default port of Cloud Run. See Terraform script (infra/terraform/api/main.tf) for more information.
 docker run -it -d -p 4000:8080 $IMAGE_NAME:$IMAGE_VERSION
+# If use the image in GCP Container Registory
+# ref: https://cloud.google.com/container-registry/docs/advanced-authentication#gcloud-helper
+# docker pull gcr.io/sdvx-score-rader/api-server:latest
 
 # Log
 # docker logs -f --tail=500 <Container Id>
